f5495bc8bdb65077f0dc16337cf63b9f3dad2df5
begemotv <62621854+begemotv@users.noreply.github.com>
2020-11-27 20:17:50 +0200

    TLD тесты добавлены

---
+++ src/components/question-genre-screen/question-genre-screen.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import QuestionGenreScreen from './question-genre-screen';
+
+const question = {
+  type: `genre`,
+  genre: `rock`,
+  answers: [{
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `rock`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `blues`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `jazz`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `rock`,
+  }],
+};
+
+const noop = () => {};
+
+it(`QuestionGenreScreen is rendered correctly`, () => {
+  const tree = renderer.create((
+    <QuestionGenreScreen
+      question={question}
+      onAnswer={noop}
+      renderPlayer={noop}
+      onChange={noop}
+      userAnswers={[false, false, false, false]}
+    >
+      <React.Fragment />
+    </QuestionGenreScreen>
+  ), {
+    createNodeMock: () => {
+      return {};
+    }
+  }).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
